31896a0d407aeb30e609eea060fa8cfb07dfd718
tomoyuki.sato <funkyfunkytomo@gmail.com>
2024-01-06 16:47:31 +0900

    TYPO修正

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,16 @@
-import NextAuth from "next-auth/next";
-import GithubProvier from "next-auth/providers/github";
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 
 export const authOptions = {
     secret: process.env.NEXTAUTH_SECRET,
     providers: [
-        GithubProvier({
+        GithubProvider({
             clientId: process.env.GITHUB_CLIENT_ID ?? "",
-            clientSecret: process.env.GITHUB_CLEINT_SECRET ?? "",
+            clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
         }),
     ],
 };
 
 const handler = NextAuth(authOptions);
+
 export { handler as GET, handler as POST };
